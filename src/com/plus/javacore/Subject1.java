9078080ffc1d5b2749f80f74ff8f1b44914ca369
unknown <anhdt84@gmail.com>
2020-09-26 20:35:37 +0700

    Khoi tao repo

---
+++ src/com/plus/javacore/Subject1.java
@@ -0,0 +1,10 @@
+package com.plus.javacore;
+
+/**
+ * Created by Mina Mimi on 9/26/2020.
+ */
+public class Subject1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
