b40fdb19af0c6312f7ce14e3ccb5d90b776c4f1d
unknown <anhdt84@gmail.com>
2020-10-15 12:15:22 +0700

    fix

--- src/com/plus/practice/exercises/ex5/ArrayDup.java
+++ src/com/plus/practice/exercises/ex5/ArrayDup.java
@@ -1,5 +1,6 @@
 package com.plus.practice.exercises.ex5;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -9,7 +10,10 @@ public class ArrayDup {
     public static void main(String[] args) {
         int arr[] = {10,20,20,30,30,50,48,50};
         int length = arr.length;
+        //Buoc 1: Sap xep lai mang
         Arrays.sort(arr);
+
+        //Buoc 2: remove cac gia tri trung nhau
         int j = 0;
         for (int i = 0; i < length - 1; i++) {
             if(arr[i] != arr[i+1]) {
@@ -17,8 +21,9 @@ public class ArrayDup {
             }
         }
         arr[j] = arr[length-1];
-        int[] newArray = new int[j+1];
 
+        //Copy vao mang moi
+        int[] newArray = new int[j+1];
         for (int i = 0; i <= j ; i++) {
             newArray[i] = arr[i];
         }
