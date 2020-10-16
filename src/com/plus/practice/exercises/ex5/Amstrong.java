623b8680727b0bb3047ef8001ecacb080f61b8fa
unknown <anhdt84@gmail.com>
2020-10-16 14:30:19 +0700

    fix

--- src/com/plus/practice/exercises/ex5/Amstrong.java
+++ src/com/plus/practice/exercises/ex5/Amstrong.java
@@ -1,5 +1,7 @@
 package com.plus.practice.exercises.ex5;
 
+import java.util.Scanner;
+
 /**
  * Created by Mina Mimi on 10/14/2020.
  */
@@ -27,19 +29,51 @@ public class Amstrong {
 //        }
 //    }
 
-    //Cach 2
-    public static void main(String[] args) {
-        for (int i = 1; i < 500; i++) {
-            char[] charArray = String.valueOf(i).toCharArray();
-            int pow = charArray.length;
-            int sum = 0;
-            for(char myNum:charArray) {
-                sum = sum + (int)Math.pow(Character.getNumericValue(myNum),pow);
-            }
-            if(sum == i) {
-                System.out.println(i + " is Amstrong number");
-            }
+//    //Cach 2
+//    public static void main(String[] args) {
+//        for (int i = 1; i < 500; i++) {
+//            char[] charArray = String.valueOf(i).toCharArray();
+//            int pow = charArray.length;
+//            int sum = 0;
+//            for(char myNum:charArray) {
+//                sum = sum + (int)Math.pow(Character.getNumericValue(myNum),pow);
+//            }
+//            if(sum == i) {
+//                System.out.println(i + " is Amstrong number");
+//            }
+//
+//        }
+//    }
 
+    //Bao
+
+    public static void main(String[] args){
+        int temp1 = 0, temp2 = 0, sum = 0, element = 0;
+        System.out.println("List number Armstrong between 1 to 500:");
+        for(int i = 1; i <= 500; i++){
+            if(i <= 9){
+                if(i == i*1){
+                    System.out.print(i +" ");
+                }
+            }
+            if(i > 9 && i < 100){
+                temp1 = (i/10) * (i/10) + (i%10) *(i%10);
+                if(i == temp1){
+                    System.out.print(i +" ");
+                }
+            }
+            if (i >= 100) {
+                temp2 = i;
+                sum = 0;
+                while (temp2 != 0){
+                    element = temp2 % 10;
+                    sum += Math.pow(element, 3);
+                    temp2 /= 10;
+                }
+                if(i == sum){
+                    System.out.print(i +" ");
+                }
+            }
         }
     }
 }
