a139920085a7c9aee4602fc426b0f4e547cd0473
unknown <anhdt84@gmail.com>
2020-11-05 16:53:17 +0700

    sample IO

---
+++ src/com/plus/javacore/io/Sample.java
@@ -0,0 +1,155 @@
+package com.plus.javacore.io;
+
+import java.io.*;
+
+public class Sample {
+    public static void main(String[] args) {
+
+        /**
+         * OutputStream: Write to file
+         */
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream("abc.txt");
+            String s = "Hello";
+            byte[] bytes = s.getBytes();
+            outputStream.write(bytes);
+            outputStream.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * InputStream: Read from file su dung FileInputStream
+         */
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream("abc.txt");
+            System.out.println(inputStream.available());
+            int in;
+            while ((in = inputStream.read()) != -1 ) {
+                System.out.print((char)in);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * Read from file su dung BufferedInputStream
+         */
+        System.out.println("========================");
+        inputStream = null;
+        BufferedInputStream bf = null;
+        try {
+            inputStream = new FileInputStream("abc.txt");
+            bf = new BufferedInputStream(inputStream);
+            int in;
+            while ((in = bf.read()) != -1 ) {
+                System.out.print((char)in);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bf.close();
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * Read file from InputStreamReader
+         */
+        try {
+            inputStream = new FileInputStream("abc.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            int read = 0;
+            while ((read = inputStreamReader.read()) != -1) {
+                System.out.print((char)read);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        /**
+         * ObjectInputStream: write
+         */
+        ObjectOutput objectOutput = null;
+        try {
+            objectOutput = new ObjectOutputStream(new FileOutputStream("abc.txt"));
+            Student student = new Student("SV1",12);
+            objectOutput.writeObject(student);
+            System.out.println("Finished");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                objectOutput.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * ObjectInputStream: read
+         */
+        ObjectInputStream objectInputStream = null;
+        try {
+            objectInputStream = new ObjectInputStream(new FileInputStream("abc.txt"));
+            Student student = (Student) objectInputStream.readObject();
+            System.out.println("Finished");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                objectInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * BufferReader
+         */
+        System.out.println("=====================BufferReader=======================");
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("test.txt"));
+            //Cach 1:
+//            String line;
+//            while((line = br.readLine()) != null) {
+//                System.out.println(line);
+//            }
+            //Cach 2:
+            int mychar;
+            while((mychar = br.read()) != -1) {
+                System.out.print((char)mychar);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
---
+++ src/com/plus/javacore/io/Student.java
@@ -0,0 +1,36 @@
+package com.plus.javacore.io;
+
+import java.io.Serializable;
+
+public class Student implements Comparable<Student>, Serializable {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        //So sanh theo ten hoac tuoi:
+        //return this.name.compareTo(o.name);
+        if(this.age > o.age) {
+            return 1;
+        } else if (this.age < o.age) {
+            return -1;
+        } else {
+            return 0;
+        }
+
+
+    }
+}
